fcc426b238f3416c67dcc5760e68ca6189404215
EduardoSantos273 <95573391+EduardoSantos273@users.noreply.github.com>
2024-08-22 11:13:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function adicionarCliente() {
+    document.getElementById('calculadora').style.display = 'block';
+}
+
+function excluirCliente() {
+    // Lógica para excluir cliente
+}
+
+function editarCliente() {
+    // Lógica para editar cliente
+}
+
+function adicionarItem(item, preco) {
+    // Lógica para adicionar item à calculadora
+    console.log(`Item: ${item}, Preço: ${preco}`);
+}
+v
